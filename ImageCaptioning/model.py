d55d87e9cb2c5a9812ce950190e0102b487c2b11
ruslanhalitov <ruslan2439@gmail.com>
2020-10-14 21:21:50 +0200

    added LSTM char generator

--- ImageCaptioning/model.py
+++ ImageCaptioning/model.py
@@ -24,6 +24,7 @@ class EncoderCNN(nn.Module):
 
             return self.dropout(self.relu(features))
 
+
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
         super(DecoderRNN, self).__init__()
