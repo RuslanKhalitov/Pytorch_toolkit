66dfb69080d97ef09d5c01946fe9c1a6e1caf6fd
ruslanhalitov <ruslan2439@gmail.com>
2020-10-16 14:51:22 +0200

    torchtext

--- attention_transformer/torchtext_custom_datasets.py
+++ attention_transformer/torchtext_custom_datasets.py
@@ -3,8 +3,15 @@
 # 3. Construct an iterator to do batching and padding -> BucketIterator
 
 from torchtext.data import Field, TabularDataset, BucketIterator
+import torch
+import spacy
+spacy_en = spacy.load('en')
 
-tokenize = lambda x: x.split()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def tokenize(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+# tokenize = lambda x: x.split()
 
 quote = Field(sequential=True, use_vocab=True, tokenize=tokenize, lower=True)
 score = Field(sequential=False, use_vocab=False)
@@ -12,10 +19,10 @@ score = Field(sequential=False, use_vocab=False)
 fields = {'quote': ('q', quote), 'score': ('s', score)}
 
 train_data, test_data = TabularDataset.splits(
-                                            path='dataset/torchtext_test_dataset',
+                                            path='torchtext_test_dataset',
                                             train='train.json',
                                             test='test.json',
-                                            # validation = 'validation.json'
+                                            # validation = 'validation.json',
                                             format='json',
                                             fields=fields
                                         )
@@ -36,6 +43,21 @@ train_data, test_data = TabularDataset.splits(
 #                                             fields=fields
 #                                         )
 
-print(train_data[0].__dict__.keys())
-print(train_data[0].__dict__.values())
+# print(train_data[0].__dict__.keys())
+# print(train_data[0].__dict__.values())
+
+quote.build_vocab(train_data,
+                  max_size=10000,
+                  min_freq=1,
+                  vectors='glove.6B.100d') #1GB
+
+train_iterator, test_iterator = BucketIterator.splits(
+    (train_data, test_data),
+    batch_size=2,
+    device=device
+)
+
+for batch in train_iterator:
+    print(batch.q)
+    print(batch.s)
 
